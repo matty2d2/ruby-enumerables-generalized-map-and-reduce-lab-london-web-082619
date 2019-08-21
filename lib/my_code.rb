f762fede4e9fa702dc4e65544883081afbf088ab
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-21 11:20:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
-# Your Code Here
+def map(source_array)
+  new_Array = []
+  source_array.each do |x|
+    new_Array.push(yield(x))
+  end
+  new_Array
+end
+
+def reduce(source_array, starting_point = 0)
+   i = starting_point
+  source_array.each do |x|
+    i = yield(i,x)
+  end
+  i
+end
