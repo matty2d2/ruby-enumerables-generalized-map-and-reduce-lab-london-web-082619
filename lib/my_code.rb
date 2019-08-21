42ff337b5007f18219042c011a9f27567a287cad
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-21 11:47:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,7 @@ end
 def reduce(source_array, starting_point = 0)
    i = starting_point
   source_array.each do |x|
-    if !!i == false
+    if yield(i,x) == 0
       return false
     else
       i = yield(i,x)
