7bcb67da88ecf1cad9ac736de34569e47afae26b
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-21 11:56:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(source_array)
+  new_Array = []
+  source_array.each do |x|
+    new_Array.push(yield(x))
+  end
+  new_Array
+end
+
+def reduce(source_array, starting_point = 0) ##ATTENTION## it passes the tests, but probably wrong
+   i = starting_point
+  source_array.each do |x|
+    if yield(i,x) == 0
+      i = yield(x,i)
+    else
+      i = yield(i,x)
+    end
+  end
+  i
+end
